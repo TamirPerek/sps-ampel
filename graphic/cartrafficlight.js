d261a584094a24734a97894cef800c5598b7684c
Nils Brödner <nilsbroedner@me.com>
2019-05-08 18:05:04 +0200

    aufräumen

--- graphic/cartrafficlight.js
+++ graphic/cartrafficlight.js
@@ -6,7 +6,7 @@ class CarTrafficLight {
         this.orientation = orientation;
         this.margin = margin;
         this.red = false;
-        this.yellow = false;
+        this.yellow =false;
         this.green = false;
     }
 
@@ -28,18 +28,18 @@ class CarTrafficLight {
             circle(this.x + (this.margin * 4), this.y - this.margin, this.diameter);
         } else if (this.orientation === "up") {
             fill(255, 0, 0, this.red ? 255 : 50);
-            circle(this.x - this.margin, this.y - (this.margin * 2), this.diameter);
+            circle(this.x - this.margin, this.y - (this.margin * 2.25), this.diameter);
             fill(255, 255, 0, this.yellow ? 255 : 50);
-            circle(this.x - this.margin, this.y - (this.margin * 3), this.diameter);
+            circle(this.x - this.margin, this.y - (this.margin * 3.25), this.diameter);
             fill(0, 255, 0, this.green ? 255 : 50);
-            circle(this.x - this.margin, this.y - (this.margin * 4), this.diameter);
+            circle(this.x - this.margin, this.y - (this.margin * 4.25), this.diameter);
         } else if (this.orientation === "down") {
             fill(255, 0, 0, this.red ? 255 : 50);
-            circle(this.x + this.margin, this.y + (this.margin * 2), this.diameter);
+            circle(this.x + this.margin, this.y + (this.margin * 2.25), this.diameter);
             fill(255, 255, 0, this.yellow ? 255 : 50);
-            circle(this.x + this.margin, this.y + (this.margin * 3), this.diameter);
+            circle(this.x + this.margin, this.y + (this.margin * 3.25), this.diameter);
             fill(0, 255, 0, this.green ? 255 : 50);
-            circle(this.x + this.margin, this.y + (this.margin * 4), this.diameter);
+            circle(this.x + this.margin, this.y + (this.margin * 4.25), this.diameter);
         }
     }
 
--- sketch.js
+++ sketch.js
@@ -1,10 +1,9 @@
-const streetWidth = 150;
-const margin = 50;
+// const streetWidth = 150;
+// const margin = ((width + height) / 2) / 16;
 
-// Trafficlights
 var carTrafficLight = new Array();
-var pedestrianTrafficLight = new Array();
-var constructionTrafficLight = new Array();
+
+var pedestrianTrafficLight = new Array();;
 
 var street;
 
@@ -16,15 +15,26 @@ require('electron').ipcRenderer.on('update-value', function (event, values) {
 
 function setup() {
 
-  createCanvas(800, 800);
+  // createCanvas(1024, 1024);
+  createCanvas(windowWidth < windowHeight ? windowWidth : windowHeight, windowWidth < windowHeight ? windowWidth : windowHeight);
 
-  carTrafficLight.push(new CarTrafficLight(width / 2 - streetWidth, height / 2 - streetWidth, 40, "up", margin));
-  carTrafficLight.push(new CarTrafficLight(width / 2 + streetWidth, height / 2 - streetWidth, 40, "right", margin));
-  carTrafficLight.push(new CarTrafficLight(width / 2 + streetWidth, height / 2 + streetWidth, 40, "down", margin));
-  carTrafficLight.push(new CarTrafficLight(width / 2 - streetWidth, height / 2 + streetWidth, 40, "left", margin));
+  const margin = ((width + height) / 2) / 16;
+  const streetWidth = ((width + height) / 2) / 5;
 
-  pedestrianTrafficLight.push(new PedestrianTrafficLight(width / 2 - streetWidth, height / 2 + streetWidth, 15, "left-up", 30));
-  pedestrianTrafficLight.push(new PedestrianTrafficLight(width / 2 - streetWidth, height / 2 + streetWidth, 15, "left-right", 30));
+  carTrafficLight.push(new CarTrafficLight(width / 2 - streetWidth, height / 2 - streetWidth, ((width + height) / 2) / 20, "up", margin));
+  carTrafficLight.push(new CarTrafficLight(width / 2 + streetWidth, height / 2 - streetWidth, ((width + height) / 2) / 20, "right", margin));
+  carTrafficLight.push(new CarTrafficLight(width / 2 + streetWidth, height / 2 + streetWidth, ((width + height) / 2) / 20, "down", margin));
+  carTrafficLight.push(new CarTrafficLight(width / 2 - streetWidth, height / 2 + streetWidth, ((width + height) / 2) / 20, "left", margin));
+
+  
+  pedestrianTrafficLight.push(new PedestrianTrafficLight(width / 2 - streetWidth, height / 2 - streetWidth, ((width + height) / 2) / 50, "up-right", margin/2));
+  pedestrianTrafficLight.push(new PedestrianTrafficLight(width / 2 + streetWidth, height / 2 - streetWidth, ((width + height) / 2) / 50, "right-left", margin/2));
+  pedestrianTrafficLight.push(new PedestrianTrafficLight(width / 2 + streetWidth, height / 2 - streetWidth, ((width + height) / 2) / 50, "right-down", margin/2));
+  pedestrianTrafficLight.push(new PedestrianTrafficLight(width / 2 + streetWidth, height / 2 + streetWidth, ((width + height) / 2) / 50, "down-up", margin/2));
+  pedestrianTrafficLight.push(new PedestrianTrafficLight(width / 2 + streetWidth, height / 2 + streetWidth, ((width + height) / 2) / 50, "down-left", margin/2));
+  pedestrianTrafficLight.push(new PedestrianTrafficLight(width / 2 - streetWidth, height / 2 + streetWidth, ((width + height) / 2) / 50, "left-right", margin/2));
+  pedestrianTrafficLight.push(new PedestrianTrafficLight(width / 2 - streetWidth, height / 2 + streetWidth, ((width + height) / 2) / 50, "left-up", margin/2));
+  pedestrianTrafficLight.push(new PedestrianTrafficLight(width / 2 - streetWidth, height / 2 - streetWidth, ((width + height) / 2) / 50, "up-down", margin/2));
 
   street = new Street(width, height, streetWidth);
 }
@@ -48,9 +58,24 @@ function draw() {
     light.draw();
   }
 
+  // Update States
+  update();
+  
+}
+
+function update() {
   if (myValues !== null) {
+    // Update Car Trafficlights
     for (let i = 1; i <= 4; i++) {
       carTrafficLight[i - 1].update(myValues['Lampe H' + i + '.1'], myValues['Lampe H' + i + '.2'], myValues['Lampe H' + i + '.3']);
     }
+
+    // Update Pedestrian Trafficlights
+    for (let i = 1; i <= 8; i++) {
+      pedestrianTrafficLight[i - 1].update(myValues['Lampe H' + i + '.7'], myValues['Lampe H' + i + '.8']);
+      pedestrianTrafficLight[i].update(myValues['Lampe H' + i + '.7'], myValues['Lampe H' + i + '.8']);
+      i++;
+    }
+
   }
 }
